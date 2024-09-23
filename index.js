5e02c6bc8a09a895afb16881542f6adf323484ba
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2024-09-23 15:08:24 +0000

    nav responsive

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
 let elements = [
-    { id: "#skillOne", start: 0, end: 60, duration: 4000 },
-    { id: "#skillTwo", start: 0, end: 40, duration: 4000 },
-    { id: "#skillThree", start: 0, end: 50, duration: 4000 },
-    { id: "#skillFour", start: 0, end: 10, duration: 4000 },
-    { id: "#skillFive", start: 0, end: 5, duration: 4000 }
+  { id: "#skillOne", start: 0, end: 60, duration: 4000 },
+  { id: "#skillTwo", start: 0, end: 40, duration: 4000 },
+  { id: "#skillThree", start: 0, end: 50, duration: 4000 },
+  { id: "#skillFour", start: 0, end: 10, duration: 4000 },
+  { id: "#skillFive", start: 0, end: 5, duration: 4000 },
 ];
 
 function animateValue(id, start, end, duration) {
-    let obj = document.querySelector(id);
-    let startTimestamp = null;
-    function step(timestamp) {
-        if (startTimestamp === null)
-            startTimestamp = timestamp;
-        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-        obj.innerHTML = Math.floor(progress * (end - start) + start);
-        if (progress < 1) {
-            window.requestAnimationFrame(step);
-        }
-    };
-    window.requestAnimationFrame(step);
+  let obj = document.querySelector(id);
+  let startTimestamp = null;
+  function step(timestamp) {
+    if (startTimestamp === null) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  }
+  window.requestAnimationFrame(step);
 }
 
 function startCounterWhenVisible(id, start, end, duration) {
-    const target = document.querySelector(id);
-    
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                animateValue(id, start, end, duration);
-            }
-        });
-    }, { threshold: 0.5 }); 
-    observer.observe(target);
+  const target = document.querySelector(id);
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          animateValue(id, start, end, duration);
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+  observer.observe(target);
 }
 
-window.onload = function() {
-    elements.forEach(({ id, start, end, duration }) => {
-        startCounterWhenVisible(id, start, end, duration);
-    });
+window.onload = function () {
+  elements.forEach(({ id, start, end, duration }) => {
+    startCounterWhenVisible(id, start, end, duration);
+  });
 };
+
+
+function toggleMenu(event) {
+    event.preventDefault();
+    let navItems = document.querySelector(".nav-items");
+    navItems.classList.toggle("show");
+
+    console.log(navItems)
+}
